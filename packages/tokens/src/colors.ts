ab8a0fe770cd6af05c149353d9448dc0b88e2817
dsllt <dayanesallet@hotmail.com>
2023-07-03 17:13:56 -0300

    Cria o package de tokens

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,23 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#F8F9FC',
+  gray200: '#E6E8F2',
+  gray300: '#D1D6E4',
+  gray400: '#8FB6FA',
+  gray500: '#80A3E0',
+  gray600: '#6A87BA',
+  gray700: '#46597A',
+  gray800: '#344460',
+  gray900: '#212B3B',
+
+  green100: '#CAFBAD',
+  green200: '#4B7A30',
+  green300: '#627A54',
+
+  orange100: '#F4A35E',
+  orange200: '#E3791E',
+
+  test: '#FFF',
+}
---
+++ packages/tokens/src/radii.ts
@@ -0,0 +1,8 @@
+export const radii = {
+  '1p': '1px',
+  '4p': '4px',
+  '6p': '6px',
+  '8p': '8px',
+  '16p': '16px',
+  full: '99999px',
+}
