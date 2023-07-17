d6d6af20574a7a2d9a2e6fe384dd4a03db6a5fa5
dsllt <dayanesallet@hotmail.com>
2023-07-17 11:34:39 -0300

    Improment in InputTExt

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,16 +1,18 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, ElementRef, forwardRef } from 'react'
 import { Input, TextInputContainer } from './styles'
 import { MagnifyingGlass } from 'phosphor-react'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {}
 
-export function TextInput({ ...props }: TextInputProps) {
-  return (
-    <TextInputContainer>
-      <Input {...props} />
-      <MagnifyingGlass size={20} />
-    </TextInputContainer>
-  )
-}
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ ...props }, ref) => {
+    return (
+      <TextInputContainer>
+        <Input ref={ref} {...props} />
+        <MagnifyingGlass size={20} />
+      </TextInputContainer>
+    )
+  },
+)
 
 TextInput.displayName = 'TextInput'
